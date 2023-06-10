9861723d7f28e688e403fb06c20d7acc76c32227
Tirth Vora <tirthdatascience@gmail.com>
2023-06-11 01:31:43 +0530

    Web app deployment on localhost successful

--- app.py
+++ app.py
@@ -32,6 +32,18 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regression_model.predict(final_input)[0]
+    scaled_output = round(output * 1000)
+    return render_template("home.html", prediction_text = 'The predicted house price is: {}'.format(scaled_output))
+
+
+     
+
 if __name__ == "__main__":
     app.run(debug=True)
 
